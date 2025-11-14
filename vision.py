29b3df9fec53b7a57684bd8e2d78d35a26563a3a
007 <007lookworld@gmail.com>
2025-11-14 12:25:26 +0800

    feat(目标检测): 添加锚框偏移量计算和逆向转换函数
    实现offset_boxes函数计算锚框与真实边界框的偏移量
    实现offset_inverse函数将预测偏移量应用于锚框生成预测边界框

---
+++ vision.py
@@ -0,0 +1,52 @@
+
+
+# 求锚框和真实边界框的偏移量
+# anchors(NAC,4):(xmin, ymin, xmax, ymax)
+# target_anchors(NAC,4):(xmin, ymin, xmax, ymax)
+def offset_boxes(anchors, target_anchors, eps=1e-6):
+    # 一、将锚框从角点格式转换为中心点格式
+    # c_anchors: (NAC, 4):(x, y, w, h)
+    # c_target_anchors: (NAC, 4):(x, y, w, h)
+    c_anchors = d2l.box_corner_to_center(anchors)
+    c_target_anchors = d2l.box_corner_to_center(target_anchors)
+
+    # 二、归一化(中心点和宽高的偏移)
+    # offset_xy(NAC, 2):(dx, dy)
+    # offset_wh(NAC, 2):(dw, dh)
+    offset_xy = (c_target_anchors[:, :2] - c_anchors[:, :2]) / c_anchors[:, 2:]
+    offset_wh = torch.log(eps + c_target_anchors[:, 2:] / c_anchors[:, 2:])
+
+    # 三、标准化(中心点和宽高的偏移)
+    # offset_xy(NAC, 2):(tx, ty)
+    # offset_wh(NAC, 2):(tw, th)
+    offset_xy = offset_xy * 10
+    offset_wh = offset_wh * 5
+
+    # 四、拼接中心点偏移和宽高偏移，形成最终偏移量张量
+    # offset(NAC, 4):(tx, ty, tw, th)
+    offset = torch.cat([offset_xy, offset_wh], axis=1)
+    return offset
+
+
+
+
+# 把偏移量施加到锚框上，得到预测框
+# anchor(NAC,4):(xmin,ymin,xmax,ymax)
+# offset(NAC,4):(tx,ty,tw,th)
+def offset_inverse(anchors, offset):
+    """根据带有预测偏移量的锚框来预测边界框"""
+    # anchors(NAC,4):(xmin,ymin,xmax,ymax)
+    # c_anchors(NAC,4):(x,y,w,h)
+    c_anchors = d2l.box_corner_to_center(anchors)
+    
+    # c_target_anchor_xy(NAC,2):(x,y):施加偏移量后的物理坐标
+    c_target_anchor_xy = (offset[:, :2] * c_anchors[:, 2:] / 10) + c_anchors[:, :2]
+    # c_target_anchor_wh(NAC,2):(w,h):施加偏移量后的物理尺寸
+    c_target_anchor_wh = torch.exp(offset[:, 2:] / 5) * c_anchors[:, 2:]
+
+    # c_target_anchor(NAC,4):(x,y,w,h)
+    c_target_anchor = torch.cat((c_target_anchor_xy, c_target_anchor_wh), axis=1)
+    # target_anchor(NAC,4):(xmin,ymin,xmax,ymax)
+    target_anchor = d2l.box_center_to_corner(c_target_anchor)
+    # target_anchor(NAC,4):(xmin,ymin,xmax,ymax)
+    return target_anchor
