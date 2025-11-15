d5a6a2838aa198ee5bb0967904b05affef7dac0e
007 <007lookworld@gmail.com>
2025-11-15 13:39:48 +0800

    refactor(nms): 优化非极大值抑制算法实现
    重构NMS算法实现，调整代码结构以提高可读性。主要修改包括：
    1. 调整剩余框索引计算顺序，使其逻辑更清晰
    2. 优化索引计算方式，直接使用相对索引
    3. 添加更详细的算法说明注释

--- common/nms.py
+++ common/nms.py
@@ -2,6 +2,7 @@ import torch
 from .iou import iou
 '''
     非极大值抑制非极大值抑制:删除置信度接近的锚框
+    保留置信度最高的检测框，并抑制与其高度重叠（即交并比 IoU 较大）的其他检测框。
 '''
 
 
@@ -16,16 +17,17 @@ def nms(anchors, scores, iou_threshold):
 
     # NMS
     while anchor_inds.numel() > 0:# 当待处理框数量大于0时，继续处理
-        # 当前最高置信框原始索引
+        # 当前列表中最高置信框原始索引
         max_score_id = anchor_inds[0]
-        # 剩余待比较框索引
-        rest_anchor_inds = anchor_inds[1:]
         # 保留当前最高置信框索引
         keep.append(max_score_id)
         # 如果只剩一个框，直接跳出循环
         if anchor_inds.numel() == 1: 
             break
         
+        # 剩余待比较框索引
+        rest_anchor_inds = anchor_inds[1:]
+
         # 取出当前最高置信框
         # max_score_anchor: (1, 4)
         max_score_anchor = anchors[max_score_id, :].reshape(-1, 4)
@@ -34,16 +36,16 @@ def nms(anchors, scores, iou_threshold):
         rest_anchors = anchors[rest_anchor_inds, :].reshape(-1, 4)
 
         # anchor_iou: (1,N-1)->(N-1,)
+        # 此时anchor_iou并不是从大到小顺序序列
         anchor_iou = iou(max_score_anchor, rest_anchors).reshape(-1)
         # 筛选出 IoU 小于阈值的框自然索引,保留，删除太相似的anchor
         # inds: (M,)  保留的索引，M <= N-1
         # torch.nonzero返回的是满足条件的自然索引，从0开始
         # 但是torch.nonzero返回的shape是(M,1)，需要reshape(-1)
         inds = torch.nonzero(anchor_iou <= iou_threshold).reshape(-1)
-        # 转换为总绝对索引
-        inds = inds + 1
-        # 下一轮参与NMS的框索引(一直沿用原始索引)
-        anchor_inds = anchor_inds[inds]
+        # ins+1:0号是当前最高置信框
+        # 更逊anchor索引：去掉本轮最大置信框和相似框，保留其他框，用于下一轮计算
+        anchor_inds = anchor_inds[inds+1]
     # keep(k,): 最终保留的框索引(原始索引)
     keep = torch.tensor(keep, device=anchors.device)
     return keep
